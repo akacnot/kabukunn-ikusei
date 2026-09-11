0d70c7a3486f10eca378e4eee1c8a2a964bc8522
akacnot <158011397+akacnot@users.noreply.github.com>
2026-09-11 22:59:25 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD_kdD-STQWAQVhMHeHfXchW5e74ESrNoI",
+  authDomain: "kabukunn-28e85.firebaseapp.com",
+  projectId: "kabukunn-28e85",
+  storageBucket: "kabukunn-28e85.firebasestorage.app",
+  messagingSenderId: "161727203465",
+  appId: "1:161727203465:web:4af5fecf60c5c3da635e58",
+  measurementId: "G-WQBBEE0GEF"
+};
